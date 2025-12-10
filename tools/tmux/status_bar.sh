7afc3af0fc64bc7899641bfa6fdd9adcdf1140fe
Shayan Haghighi <shayan.haghighi04@gmail.com>
2025-12-10 14:13:10 +0000

    update tmux conf

--- tools/tmux/status_bar.sh
+++ tools/tmux/status_bar.sh
@@ -13,19 +13,33 @@ build_module() {
     echo "#[fg=${color},bg=default]${sep_l}#[fg=${thm_base},bg=${color}]${icon} #[fg=${thm_text},bg=${thm_surface0}] ${text}#[fg=${thm_surface0},bg=default]${sep_r}"
 }
 
-mod_git=$(build_module "$thm_peach" "󰊢" "#(gitmux -cfg $XDG_CONFIG_HOME/tmux/.gitmux.conf #{pane_current_path})")
+mod_git=$(build_module "$thm_red" "󰊢" "#(gitmux -cfg $XDG_CONFIG_HOME/tmux/.gitmux.conf #{pane_current_path})")
 
 mod_session=$(build_module "#{?client_prefix,$thm_red,$thm_green}" "" "#S ")
 
 mod_date=$(build_module "$thm_red" "" "%H:%M")
-mod_dir=$(build_module "$thm_red" "" "#(get-dir #{pane_current_path})")
 
 tmux set -g status-left-length 100 
 tmux set -g status-right-length 100 
 
 tmux set -g status-bg "$thm_black"
-tmux set -g status-right "${mod_git} ${mod_dir}"
+tmux set -g status-right "${mod_git} "
 tmux set -g status-left "${mod_session}     "
+
+tmux set -g @window_flags "none"
+tmux set -g @window_flags_icon_last "󰖰" # -
+tmux set -g @window_flags_icon_zoom "󰁌" # Z
+tmux set -g @window_flags_icon_mark "󰃀" # M
+tmux set -g @window_flags_icon_silent "󰂛" # ~
+tmux set -g @window_flags_icon_activity "󱅫" # #
+tmux set -g @window_flags_icon_bell "󰂞" # !
+tmux set -g @window_flags_icon_format "\
+#{E:?window_activity_flag,#{E:@window_flags_icon_activity},}\
+#{E:?window_bell_flag,#{E:@window_flags_icon_bell},}\
+#{E:?window_silence_flag,#{E:@window_flags_icon_silent},}\
+#{E:?window_last_flag,#{E:@window_flags_icon_last},}\
+#{E:?window_marked_flag,#{E:@window_flags_icon_mark},}\
+#{E:?window_zoomed_flag,#{E:@window_flags_icon_zoom},}"
 
 tmux setw -g window-status-format " 
 #[fg=$thm_mauve,bg=default]$sep_l\
@@ -37,6 +51,8 @@ tmux setw -g window-status-current-format " \
 #[fg=$thm_blue,bg=default]$sep_l\
 #[fg=$thm_base,bg=$thm_blue]#I \
 #[fg=$thm_black,bg=$thm_blue,bold] #W \
+#{E:@catppuccin_window_flags_icon_format}\
 #[fg=$thm_blue,bg=default]$sep_r"
+
 
 tmux set -g status-bg "$thm_base"
